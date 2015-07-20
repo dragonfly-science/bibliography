3e47c47b054d226d2f8732504d906a30a68ad10f
Edward Abraham <edward@dragonfly.co.nz>
2015-07-21 00:24:06 +1200

    Move dragonfly specific files to the dragonfly branch

--- dragonfly-extract/parse-bib.r
+++
@@ -1,9 +0,0 @@
-source('functions.r')
-
-bib <- readLines('../mfish.bib')
-reflist <- parse_bib(bib)
-
-save(reflist, file = 'reflist.rdata')
-
-cat('\n=== Unique items:\n')
-print(data.frame(n = sort(table(unlist(sapply(reflist, names))), decreasing = T)))
