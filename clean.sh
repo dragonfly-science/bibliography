3514c3b5ddfee6d249207005b5be083c57f1dbe6
Edward Abraham <edward@dragonfly.co.nz>
2020-06-12 14:35:30 +1200

    clean file duplicated

--- clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-trap exit ERR
-bibclean dragonfly.bib >  .clean.bib
-bibtool -s -d .clean.bib > .tool.bib
-mv dragonfly.bib .dragonfly.bak.bib
-mv .tool.bib dragonfly.bib
-rm .clean.bib
-
